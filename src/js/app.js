6274b336065bc926d1033cf11390fac2bfd4230e
aaron <alkrauss48@gmail.com>
2014-08-07 09:31:51 -0500

    Add outbound.js to app.js Add comments to main js files

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
-// Make this file your only JS file
+// Make this the only JS file you include in the body
 // Include external libs with 'require' calls
 
+// Gives 'console' polyfills to browsers that don't use 'console'
 require('./lib/console-support.js');
+
+// Tracks outbound-link activity through Google Analytics
+require('./lib/outbound.js');
 
 $(document).ready(function(){
 
--- src/js/beforeBody.js
+++ src/js/beforeBody.js
@@ -1,3 +1,5 @@
-// Add any US that needs to run in the head tag
+// Make this the only JS file you include in the head
+// Add only JS that can't/shouldn't be included in the body
+// Include external libs with 'require' calls
 
 require('modernizr');
