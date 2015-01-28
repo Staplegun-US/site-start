5ff38b76c26df9bb912802ed9c7048d1f7b674b3
aaron <alkrauss48@gmail.com>
2015-01-28 11:06:30 -0600

    Update outbound script

--- src/js/lib/outbound.js
+++ src/js/lib/outbound.js
@@ -1,1 +1,36 @@
-(function(){var t=function(e,t){t.length>0?window.open(e,t):window.location.href=e};if(document.getElementsByTagName){var n=document.getElementsByTagName("a"),r=document.domain.split(".").reverse()[1]+"."+document.domain.split(".").reverse()[0];for(var i=0;i<n.length;i++){var s=typeof n[i].getAttribute("href")=="string"?n[i].getAttribute("href"):"",o=s.match(r);(s.match(/^https?\:/i)&&!o||s.match(/^mailto\:/i))&&n[i].addEventListener("click",function(e){var n=this.getAttribute("href"),r=typeof (this.getAttribute("target")=="string")?this.getAttribute("target"):"";ga("send","event","outbound","click",n,{hitCallback:t(n,r)},{nonInteraction:1});e.preventDefault()})}}})();
+function _gaLt(event){
+  var el = event.srcElement || event.target;
+
+  /* Loop up the DOM tree through parent elements if clicked element is not a link (eg: an image inside a link) */
+  while(el && (typeof el.tagName == 'undefined' || el.tagName.toLowerCase() != 'a' || !el.href)){
+    el = el.parentNode;
+  }
+
+  if(el && el.href){
+    if(el.href.indexOf(location.host) == -1){ /* external link */
+      /* HitCallback function to either open link in either same or new window */
+      var hitBack = function(link, target){
+        target ? window.open(link, target) : window.location.href = link;
+      };
+      /* link */
+      var link = el.href;
+      /* Is target set and not _(self|parent|top)? */
+      var target = (el.target && !el.target.match(/^_(self|parent|top)$/i)) ? el.target : false;
+      /* send event with callback */
+      ga(
+          "send", "event", "Outgoing Links", link,
+          document.location.pathname + document.location.search,
+          {"hitCallback": hitBack(link, target)}
+        );
+
+      /* Prevent standard click */
+      event.preventDefault ? event.preventDefault() : event.returnValue = !1;
+    }
+
+  }
+}
+
+/* Attach the event to all clicks in the document after page has loaded */
+var w = window;
+w.addEventListener ? w.addEventListener("load",function(){document.body.addEventListener("click",_gaLt,!1)},!1)
+: w.attachEvent && w.attachEvent("onload",function(){document.body.attachEvent("onclick",_gaLt)});
