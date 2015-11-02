9132ac94df0dbccb940021931421bff7e81ae77f
aaron <alkrauss48@gmail.com>
2015-11-02 14:44:38 -0600

    Add gulpfile with base sass compilation

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+
+var sass    = require('gulp-sass');
+var prefix  = require('gulp-autoprefixer');
+
+var paths = {
+  styles: {
+    src: './src/sass',
+    files: './src/sass/**/*.scss',
+    dest: './dist/css'
+  }
+}
+
+var displayError = function(error) {
+  var errorString = '[' + error.plugin + ']';
+  errorString += ' ' + error.message.replace("\n",''); // Removes new line at the end
+
+  if(error.fileName)    { errorString += ' in ' + error.fileName; }
+  if(error.lineNumber)  { errorString += ' on line ' + error.lineNumber; }
+
+  console.error(errorString);
+}
+
+gulp.task('sass', function (){
+    gulp.src(paths.styles.files)
+    .pipe(sass({
+        outputStyle: 'compressed',
+        sourceComments: 'map',
+        includePaths : [paths.styles.src]
+    }))
+    .on('error', function(err){
+        displayError(err);
+    })
+    .pipe(prefix(
+        'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
+    ))
+    .pipe(gulp.dest(paths.styles.dest))
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(paths.styles.files, ['sass'])
+});
